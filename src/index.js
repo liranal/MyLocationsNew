7977c42eca321da4a0098b6e7220cb3b2b90f7f2
liranal <liraneilary@gmail.com>
2020-06-17 18:14:49 +0300

    Add Redux Persist

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store";
+import returnStoreAndPersistor from "./store";
+
+const { store } = returnStoreAndPersistor();
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/store.js
+++ src/store.js
@@ -1,9 +1,28 @@
 import { createStore, combineReducers } from "redux";
 import CategoryReducer from "./reducers/category.reducer";
 import LocationReducer from "./reducers/location.reducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(
+/*const store = createStore(
+  combineReducers({ CategoryReducer, LocationReducer })
+);*/
+
+///////////////////////////
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
   combineReducers({ CategoryReducer, LocationReducer })
 );
 
-export default store;
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
+
+//export default store;
